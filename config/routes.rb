c93cd3c2f9c276a352925d6fb0105e8ad07bbbfd
Jason Pitts <juniorjr083@gmail.com>
2018-08-10 15:41:25 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+ # get 'index', to: 'students#index'
+  #get 'about', to: "static#about"
+  #resource :students, only: :index
+  get '/students', to: 'students#index'
 end
